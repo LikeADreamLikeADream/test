c733373b15668ec9f19447dfa3c9d173120cde1f
Lizr <lizr@samsundot.com>
2019-03-25 11:05:03 +0800

    first commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        #region Quartz定时任务框架
+        public static void Test2()
+        {
+            //创建一个调度器工厂
+            var props = new NameValueCollection
+                {
+                    { "quartz.scheduler.instanceName", "QuartzDotNetDemo" }
+                };
+            var factory = new StdSchedulerFactory(props);
+
+            //获取调度器
+            var sched = factory.GetScheduler();
+            sched.Start();
+
+            IJobDetail job = JobBuilder.Create<HelloJob>().WithIdentity("sampleJob", "sampleGroup").Build();
+
+            //3、创建一个触发器
+            ISimpleTrigger trigger = (ISimpleTrigger)TriggerBuilder.Create()
+                                                        .WithIdentity("trigger1", "group1")
+                                                        .WithSimpleSchedule(x => x.WithIntervalInSeconds(1).RepeatForever())
+                                                        .Build();
+            sched.ScheduleJob(job, trigger);
+
+        }
+        [DisallowConcurrentExecution]
+        public class HelloJob : IJob
+        {
+
+            public void Execute(IJobExecutionContext context)
+            {
+
+                Console.WriteLine("Greetings from HelloJob!");
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static ILog Logger = LogManager.GetLogger("DBFactoryInfo");
+        static void Main(string[] args)
+        {
+            Class1.Test2();
+
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
