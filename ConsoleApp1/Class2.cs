42765340d734c9e2d15568ad078ae2c8edb02d19
Lizr <414377469@qq.com>
2020-03-19 15:43:13 +0800

    123

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        
+	}
+    public static string Retstr()
+    {
+        string msg = "";
+        msg = "sfdssd";
+        return msg;
+    }
+}
